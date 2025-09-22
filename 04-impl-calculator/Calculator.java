17fa8290424e87f44929a21277e0469e13150d95
Tobias-Basilisco <tobiasbasilisco@gmail.com>
2025-09-22 20:00:11 +0200

    04 done

---
+++ 04-impl-calculator/Calculator.java
@@ -0,0 +1,27 @@
+class Calculator{
+    int nOpDone;
+    double lastRes;
+
+    void build(){
+        this.nOpDone = 0;
+        this.lastRes = 0;
+    } 
+
+    double add(double a, double b){
+        this.nOpDone ++;
+        return this.lastRes = a + b;
+    }
+
+    double sub(double a, double b){
+        this.nOpDone ++;
+        return this.lastRes = a - b;
+    }
+
+    double mul(double a, double b){
+        return this.lastRes = a * b;
+    }
+
+    double div(double a, double b){
+        return this.lastRes = a / b;
+    }
+}
